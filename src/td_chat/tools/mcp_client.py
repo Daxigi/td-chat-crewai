b73b3c7a4e97b610f704b6d4393cf4bbe328d383
Daxigi <maximilianogimenez91@gmail.com>
2025-09-18 11:11:42 -0300

    cambio en mcp_client

--- src/td_chat/tools/mcp_client.py
+++ src/td_chat/tools/mcp_client.py
@@ -62,28 +62,28 @@ def load_tools_from_mcp() -> List[BaseTool]:
                     print(error_message)
                     return error_message
 
-        # Crear una nueva clase de herramienta para cada herramienta remota
-        ToolClass = type(
-            tool_name,
-            (BaseTool,),
-            {
-                # Le decimos a Python a qué módulo pertenece esta clase
-                "__module__": __name__,
+            ToolClass = type(
+                tool_name,
+                (BaseTool,),
+                {
+                    # Le decimos a Python a qué módulo pertenece esta clase
+                    "__module__": __name__,
 
-                # Añadimos las anotaciones de tipo que Pydantic exige
-                "__annotations__": {
-                    "name": str,
-                    "description": str
-                },
+                    # Añadimos TODAS las anotaciones de tipo que Pydantic ahora exige
+                    "__annotations__": {
+                        "name": str,
+                        "description": str,
+                        "args_schema": Type[BaseModel] # <--- ¡ESTA ES LA LÍNEA QUE FALTABA!
+                    },
 
-                # Asignamos los valores a los atributos
-                "name": tool_name,
-                "description": tool_description,
-                "args_schema": DynamicArgsSchema,
-                "_run": _run_mcp_tool
-            }
-        )
-        crewai_tools.append(ToolClass())
+                    # Asignamos los valores a los atributos
+                    "name": tool_name,
+                    "description": tool_description,
+                    "args_schema": DynamicArgsSchema,
+                    "_run": _run_mcp_tool
+                }
+            )
+            crewai_tools.append(ToolClass())
 
         return crewai_tools
 
