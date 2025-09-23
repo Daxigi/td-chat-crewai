1b4d829c79a8345a2ebf281390c080390fff4fab
Daxigi <maximilianogimenez91@gmail.com>
2025-09-23 08:37:02 -0300

    g

--- src/td_chat/crew.py
+++ src/td_chat/crew.py
@@ -18,7 +18,7 @@ class TdChat():
     def __init__(self):
         self.groq_llm = ChatGroq(
             api_key=os.getenv("GROQ_API_KEY"),
-            model="groq/llama3-8b-8192"
+            model="groq/llama-3.1-8b-instant"
         )
 
     @agent
