a8626fe0ed56428a18c173483c9a3438f21b15c2
Daxigi <maximilianogimenez91@gmail.com>
2025-10-06 08:11:11 -0300

    depuracion de main

--- src/td_chat/main.py
+++ src/td_chat/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import sys
 import warnings
 
 from datetime import datetime
@@ -30,42 +29,3 @@ def run(user_question: str, chat_history: str):
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        TdChat().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        TdChat().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        TdChat().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
